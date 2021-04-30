64691a190f5dc51238c51970d6f05edfa45c5a25
dsf <fddsf>
2021-04-30 12:10:29 +0200

    Implemented Socket.io

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 http.listen(process.env.PORT || 80, function() {
      console.log('listening on *:80');
 });
@@ -14,7 +15,14 @@ app.get('/', function(req, res) {
 
    // Your code here
 
-   res.render('index', { ejs: "Some", options: 'declared' })
+   res.render('index', { ejs: "Some", options: 'declared', working: 'EJS is working :)' })
 
 
+})
+
+
+io.on('connection', socket => {
+   console.log('User connected')
+
+   // Your code here
 })
